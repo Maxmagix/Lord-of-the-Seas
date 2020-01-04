9fe8bf25e82e332f77316f9f606cdcf7e85a2553
Tapir <quentin.deregnaucourt@epitech.eu>
2020-01-04 17:56:10 +0100

    Plateau de jeu

---
+++ Assets/Prefabs/Tile.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Tile : MonoBehaviour
+{
+    public Tile topTile;
+    public Tile leftTile;
+    public Tile rightTile;
+    public Tile botTile;
+
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Assets/Scripts/MoveBoat.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveBoat : MonoBehaviour
+{
+    public Tile tile;
+    private bool selected;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        selected = false;
+        Behaviour halo = (Behaviour)GetComponent("Halo");
+        halo.enabled = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (!selected) return;
+        if (Input.GetKeyUp(KeyCode.LeftArrow))
+        {
+            this.transform.position = tile.leftTile.transform.position;
+            tile = tile.leftTile;
+        }
+        else if (Input.GetKeyUp(KeyCode.UpArrow))
+        {
+            this.transform.position = tile.topTile.transform.position;
+            tile = tile.topTile;
+        }
+        else if (Input.GetKeyUp(KeyCode.RightArrow))
+        {
+            this.transform.position = tile.rightTile.transform.position;
+            tile = tile.rightTile;
+        }
+        else if (Input.GetKeyUp(KeyCode.DownArrow))
+        {
+            this.transform.position = tile.botTile.transform.position;
+            tile = tile.botTile;
+        }
+    }
+
+    void OnMouseUp()
+    {
+        if (selected)
+        {
+            selected = false;
+            Behaviour halo = (Behaviour)GetComponent("Halo");
+            halo.enabled = false;
+        }
+        else
+        {
+            Behaviour halo = (Behaviour)GetComponent("Halo");
+            halo.enabled = true;
+            selected = true;
+        }
+    }
+}
