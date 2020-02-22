33cb63e942f83eac90380b8a9032c071b9e81b78
Maxmagix <vollat.maxime@wanadoo.fr>
2020-02-22 23:47:42 +0100

    [ADD] Working rotation

--- Assets/Scripts/Hitbox.cs
+++ Assets/Scripts/Hitbox.cs
@@ -17,55 +17,62 @@ public class Hitbox : MonoBehaviour
         lastPos = new Vector3(0,0,0);
     }
 
+    public void setHitboxTiles()
+    {
+        Vector3 actualPos = this.transform.position;
+        foreach (GameObject box in hitboxTiles) {
+            Destroy(box);
+        }
+        hitboxTiles.Clear();
+        Vector3 boxpos = this.transform.GetComponent<Gameplay.MoveBoat>().tile.transform.position;
+        for (int pos = 0; pos < size.x * size.y; pos += 1) {
+            GameObject box = Instantiate(hitboxTile, this.transform.GetChild(0).transform);
+            hitboxTiles.Add(box);
+            Vector3 s = this.transform.localScale;
+            box.transform.localScale = new Vector3(1 / s.x, 0.1f, 1 / s.z);
+            box.transform.position = boxpos;
+            float x = pos / size.x;
+            float y = pos % size.x;
+            if (rotation == 90 || rotation == 270 || rotation == -90 || rotation == -270) {
+                Debug.Log("Horizontal !");
+                x = pos % size.x;
+                y = pos / size.x;
+            }
+            if (x > 0 || y > 0) {
+                if (x % 2 == 1) {
+                    Tile nextTile = this.transform.GetComponent<Gameplay.MoveBoat>().tile;
+                    for (; x > 0; x--)
+                        nextTile = nextTile.rightTile;
+                    box.transform.position = nextTile.transform.position;
+                } else {
+                    Tile nextTile = this.transform.GetComponent<Gameplay.MoveBoat>().tile.rightTile;
+                    for (; x > 0; x--)
+                        nextTile = nextTile.leftTile;
+                    box.transform.position = nextTile.transform.position;
+                }
+                if (y % 2 == 1) {
+                    Tile nextTile = this.transform.GetComponent<Gameplay.MoveBoat>().tile;
+                    for (; y > 0; y--)
+                        nextTile = nextTile.topTile;
+                    box.transform.position = nextTile.transform.position;                
+                } else {
+                    Tile nextTile = this.transform.GetComponent<Gameplay.MoveBoat>().tile.topTile;
+                    for (; y > 0; y--)
+                        nextTile = nextTile.botTile;
+                    box.transform.position = nextTile.transform.position;
+                }
+            }
+            box.transform.position += new Vector3(0, -0.05f, 0);
+        }
+        lastPos = actualPos;
+    }
+
     void Update() {
         if (!this.gameObject.active || !this.transform.GetComponent<Gameplay.MoveBoat>().tile.gameObject.active)
             return;
         Vector3 actualPos = this.transform.position;
         if (actualPos.x != lastPos.x || actualPos.y != lastPos.y || actualPos.z != lastPos.z) {
-            foreach (GameObject box in hitboxTiles) {
-                Destroy(box);
-            }
-            hitboxTiles.Clear();
-            Vector3 boxpos = this.transform.GetComponent<Gameplay.MoveBoat>().tile.transform.position;
-            for (int pos = 0; pos < size.x * size.y; pos += 1) {
-                GameObject box = Instantiate(hitboxTile, this.transform.GetChild(0).transform);
-                hitboxTiles.Add(box);
-                Vector3 s = this.transform.localScale;
-                box.transform.localScale = new Vector3(1 / s.x, 0.1f, 1 / s.z);
-                box.transform.position = boxpos;
-                float x = pos / size.x;
-                float y = pos % size.x;
-                if (rotation == 90 || rotation == 270) {
-                    x = pos % size.x;
-                    y = pos / size.x;
-                }
-                if (x > 0 || y > 0) {
-                    if (x % 2 == 1) {
-                        Tile nextTile = this.transform.GetComponent<Gameplay.MoveBoat>().tile;
-                        for (; x > 0; x--)
-                            nextTile = nextTile.rightTile;
-                        box.transform.position = nextTile.transform.position;
-                    } else {
-                        Tile nextTile = this.transform.GetComponent<Gameplay.MoveBoat>().tile.rightTile;
-                        for (; x > 0; x--)
-                            nextTile = nextTile.leftTile;
-                        box.transform.position = nextTile.transform.position;
-                    }
-                    if (y % 2 == 1) {
-                        Tile nextTile = this.transform.GetComponent<Gameplay.MoveBoat>().tile;
-                        for (; y > 0; y--)
-                            nextTile = nextTile.topTile;
-                        box.transform.position = nextTile.transform.position;                
-                    } else {
-                        Tile nextTile = this.transform.GetComponent<Gameplay.MoveBoat>().tile.topTile;
-                        for (; y > 0; y--)
-                            nextTile = nextTile.botTile;
-                        box.transform.position = nextTile.transform.position;
-                    }
-                }
-                box.transform.position += new Vector3(0, -0.05f, 0);
-            }
-            lastPos = actualPos;
+            setHitboxTiles();
         }
     }
 }
--- Assets/Scripts/Rotate.cs
+++ Assets/Scripts/Rotate.cs
@@ -8,17 +8,17 @@ public class Rotate : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
-    public void rotate(int angle)
+    public void turnAngle(int angle)
     {
         GameObject[] boats = GameObject.FindGameObjectsWithTag("MovableBoat");
         foreach (GameObject boat in boats) {
-            if (boat.gameObject.active && boat.transform.GetComponent<MoveBoat>().selected) {
-                Quaternion rot = boat.gameObject.transform.rotation;
-                boat.gameObject.transform.rotation.Set(0 + rot.x, angle + rot.y, 0 + rot.z, 0);
-                boat.transform.GetComponent<Hitbox>().rotation += angle;
+            if (boat.transform.GetComponent<MoveBoat>().selected) {
+                boat.transform.GetComponent<Hitbox>().rotation = (angle + boat.transform.GetComponent<Hitbox>().rotation) % 360;
+                boat.gameObject.transform.rotation = Quaternion.Euler(0, boat.transform.GetComponent<Hitbox>().rotation, 0);
+                boat.transform.GetComponent<Hitbox>().setHitboxTiles();
             }
         }
     }
